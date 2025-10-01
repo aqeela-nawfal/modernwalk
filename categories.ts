8ce32a7fdc4f04cd554a7b4121e4f9a65eacde1e
Akeela Nawfal <akeelanawfal01@gmail.com>
2025-10-02 02:21:52 +0530

    Delete categories.ts

--- categories.ts
+++
@@ -1,6 +0,0 @@
-export const CATEGORY_SLUG_TO_NAME: Record<string, string> = {
-  "men-clothing": "men's clothing",
-  "women-clothing": "women's clothing",
-};
-
-export const CATEGORY_SLUGS = Object.keys(CATEGORY_SLUG_TO_NAME);
